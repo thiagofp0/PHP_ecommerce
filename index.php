2f51057901e4e5de6ef6d3bbc90d0e5ee0e6ddcf
Thiago Ferreira <thiagofp707@gmail.com>
2019-01-17 16:48:46 -0200

    Tpl admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -12,6 +13,13 @@ $app->config('debug', true);
 $app->get('/', function() { // Define a rota
     
 	$page = new Page();		//Cria uma pagina de acordo com o conteúdo indicado
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() { // Define a rota
+    
+	$page = new PageAdmin();		//Cria uma pagina de acordo com o conteúdo indicado
 	$page->setTpl("index");
 
 });
